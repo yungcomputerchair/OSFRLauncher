c5dd01fbc80f953b5b536f9778d15ca30c72570e
Gent Semaj <me@ycc.dev>
2026-07-06 15:33:37 -0700

    Fix native path separators being included in request URIs

--- src/Launcher/Helpers/UriHelper.cs
+++ src/Launcher/Helpers/UriHelper.cs
@@ -6,6 +6,6 @@ public static class UriHelper
 {
     public static string JoinUriPaths(string url, params string[] paths)
     {
-        return paths.Aggregate(url, (c, s) => $"{c.TrimEnd('/')}/{s.TrimStart('/')}");
+        return paths.Aggregate(url, (c, s) => $"{c.TrimEnd('/')}/{s.Replace('\\', '/').Trim('/')}");
     }
 }
